16d6ed1e046ea7deafbcc6851a1776c1c3b3e9c6
Ziad Ahmed <ziadahmed080@gmail.com>
2026-09-24 15:34:34 +0300

    Update marketplace listings

--- server/src/routes/listings.js
+++ server/src/routes/listings.js
@@ -1,14 +1,19 @@
-import { Router } from 'express';
+import { Router } from "express";
+
 import {
   getAllListings,
   getListing,
   createListing,
   updateListing,
-  deleteListing
-} from '../controllers/listingController.js';
+  deleteListing,
+} from "../controllers/listingController.js";
 
 const router = Router();
 
-// TODO: wire up the routes described in README.md section 3.
+router.get("/", getAllListings);
+router.get("/:id", getListing);
+router.post("/", createListing);
+router.patch("/:id", updateListing);
+router.delete("/:id", deleteListing);
 
 export default router;
